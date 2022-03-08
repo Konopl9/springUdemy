4c6d7897690c9f5de14956f11307c0198067c901
mishcma <maksym.mishchenko@storaenso.com>
2022-03-08 01:18:06 +0100

    SU-5 AOP added

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -1,6 +1,7 @@
 package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
@@ -13,8 +14,26 @@ public class MainDemoApp {
     // get the bean from, spring container
     AccountDAO accountDAO = context.getBean("accountDAO", AccountDAO.class);
 
+    // get membership ben
+    MembershipDAO membershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
     // call business method
-    accountDAO.addAccount();
+    Account account = new Account();
+    accountDAO.addAccount(account, false);
+    membershipDAO.addAccount();
+    membershipDAO.goToSleep();
+
+    System.out.println("\n let's do it againt");
+
+    // call the accountdao getter/setter methods
+    accountDAO.setName("Test");
+    accountDAO.setServiceCode("TestService");
+    String name = accountDAO.getName();
+    String code = accountDAO.getServiceCode();
+
+    // call business method
+    accountDAO.addAccount(account, true);
+    accountDAO.doWork();
 
     // close context
     context.close();
--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,57 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
+//    AOP
 
-    // this is where we add all of our related advices for logging
+//    //@Before("execution(public void add*())")
+//    //@Before("execution(* add*(com.luv2code.aopdemo.Account))")
+//    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+//    public void beforeAddAccountAdvice() {
+//    System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+//    }
 
-    @Before("execution(public void addAccount())")
+//    AOP DECLARATION
+//    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+//    private void forDaoPackage() {}
+//
+//    @Before("forDaoPackage()")
+//    public void beforeAddAccountAdvice() {
+//        System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+//    }
+//
+//    @Before("forDaoPackage()")
+//    public void performApiAnalytics() {
+//    System.out.println("\n=====>>> Performing API analytics");
+//    }
+    //    AOP DECLARATION COMBO
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    // create pointcut for getter method
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+    private void getter() {}
+
+    // create pointcut for setter method
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+    private void setter() {}
+
+    // create pointcut: include package .. exclude getter \ setter
+    @Pointcut("forDaoPackage() && !(getter() || setter()))")
+    private void forDaoPackageNoGetterSetter() {}
+
+    @Before("forDaoPackageNoGetterSetter()")
     public void beforeAddAccountAdvice() {
-    System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+        System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+    }
+
+    @Before("forDaoPackageNoGetterSetter()")
+    public void performApiAnalytics() {
+    System.out.println("\n=====>>> Performing API analytics");
     }
 }
