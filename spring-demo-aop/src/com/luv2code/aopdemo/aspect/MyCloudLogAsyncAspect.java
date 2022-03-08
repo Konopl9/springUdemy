a3971eb52187ab578e2d6c04bc1faaa437aefdc9
mishcma <maksym.mishchenko@storaenso.com>
2022-03-08 01:33:30 +0100

    SU-6 added ordering for the aspects

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(3)
-public class MyDemoLoggingAspect {
-
+@Order(1)
+public class MyCloudLogAsyncAspect {
   @Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-  public void beforeAddAccountAdvice() {
-    System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+  public void logToCloud() {
+    System.out.println("\n=====>>> Logging to cloud");
   }
 }
