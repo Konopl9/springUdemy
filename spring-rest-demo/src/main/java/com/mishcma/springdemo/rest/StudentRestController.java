3e4ac095d6cae0f755cdfa498b348ff62a7c8bc5
mishcma <maksym.mishchenko@storaenso.com>
2022-03-29 00:04:05 +0200

    SU-22 added endpoint for returning student by index

--- spring-rest-demo/src/main/java/com/mishcma/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/mishcma/springdemo/rest/StudentRestController.java
@@ -3,7 +3,9 @@ package com.mishcma.springdemo.rest;
 import com.mishcma.springdemo.entity.Student;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    // define endpoint for "/students" - return list of students
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    // @PostConstruct to load the student data
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("Maksym", "Mishchenko"));
         students.add(new Student("Artem", "Kozhokaru"));
+    }
+
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
+    }
+
+    // define endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 }
