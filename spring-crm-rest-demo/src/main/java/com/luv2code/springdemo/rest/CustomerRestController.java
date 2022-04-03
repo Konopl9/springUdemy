c13142bb6ab8d486b5d2d1b18acf5ff2713fccc4
mishcma <maksym.mishchenko@storaenso.com>
2022-04-03 13:57:47 +0200

    SU-28 added PUT action to add a new customer to the system

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +42,13 @@ public class CustomerRestController {
         // this is for a savve of new item ... instead of update
         customer.setId(0);
 
+        customerService.saveCustomer(customer);
+        return customer;
+    }
+
+    // add PUT method
+    @PutMapping("/customers")
+    public Customer updateCustomer(@RequestBody Customer customer ) {
         customerService.saveCustomer(customer);
         return customer;
     }
