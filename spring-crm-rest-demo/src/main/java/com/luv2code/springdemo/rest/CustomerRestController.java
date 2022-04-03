4cc139c8b44f7ef5918673bfb4d01e03aa67c1c0
mishcma <maksym.mishchenko@storaenso.com>
2022-04-03 13:53:03 +0200

    SU-27 added POST action to add a new customer to the system

--- spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,6 +31,17 @@ public class CustomerRestController {
         if(customer == null) {
             throw new CustomerNotFoundException("Customer id not found - " + customerId);
         }
+        return customer;
+    }
+
+    // add POST method
+    @PostMapping("/customers")
+    public Customer addCustomer(@RequestBody Customer customer ) {
+        // also just in case the pass an id in JSON .. set id to 0
+        // this is for a savve of new item ... instead of update
+        customer.setId(0);
+
+        customerService.saveCustomer(customer);
         return customer;
     }
 }
