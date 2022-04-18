1abdf31866705a6905a7ab700eeefa7d32600a66
mishcma <maksym.mishchenko@storaenso.com>
2022-04-18 17:17:11 +0200

    SU-42 spring data REST applied to project, deleted all class layers

--- spring-boot-rest-api/src/main/java/com/mishcma/springboot/springbootrestapi/dao/EmployeeRepository.java
+++ spring-boot-rest-api/src/main/java/com/mishcma/springboot/springbootrestapi/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.mishcma.springboot.springbootrestapi.dao;
 
 import com.mishcma.springboot.springbootrestapi.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// Change path from employees to members
+// @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
