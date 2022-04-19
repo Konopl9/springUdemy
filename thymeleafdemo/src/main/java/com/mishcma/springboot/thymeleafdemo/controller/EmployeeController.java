f26b357510af0130b2f7c4347a5989931b69ebcb
mishcma <maksym.mishchenko@storaenso.com>
2022-04-20 00:55:22 +0200

    SU-46 added save action

--- thymeleafdemo/src/main/java/com/mishcma/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo/src/main/java/com/mishcma/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -2,36 +2,57 @@ package com.mishcma.springboot.thymeleafdemo.controller;
 
 import com.mishcma.springboot.thymeleafdemo.entity.Employee;
 import com.mishcma.springboot.thymeleafdemo.service.EmployeeService;
-import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private EmployeeService employeeService;
+  Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
-    @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+  private final EmployeeService employeeService;
+  // load employee data
+  private List<Employee> employees;
 
-    // load employee data
-    private List<Employee> employees;
+  @Autowired
+  public EmployeeController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
+  }
 
-    // add mapping for "/list"
-    @GetMapping("/list")
-    public String listEmployees(Model model) {
-        List<Employee> employees = employeeService.findAll();
+  // add mapping for "/list"
+  @GetMapping("/list")
+  public String listEmployees(Model model) {
+    List<Employee> employees = employeeService.findAll();
 
-        model.addAttribute("employees", employees);
+    model.addAttribute("employees", employees);
 
-        return "list-employees";
-    }
+    return "employees/list-employees";
+  }
+
+  @GetMapping("/showFormForAdd")
+  public String showFormForAdd(Model model) {
+    Employee employee = new Employee();
+
+    model.addAttribute("employee", employee);
+
+    return "employees/employee-form";
+  }
+
+  @PostMapping("/save")
+  public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+    logger.info(String.valueOf(employee));
+    employeeService.save(employee);
+
+    return "redirect:/employees/list";
+  }
 }
--- thymeleafdemo/src/main/java/com/mishcma/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ thymeleafdemo/src/main/java/com/mishcma/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.mishcma.springboot.thymeleafdemo.dao;
 
 import com.mishcma.springboot.thymeleafdemo.entity.Employee;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
@@ -8,4 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // @RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
